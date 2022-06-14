c85dd1b62d0ab8afc9f0fd56522efd2b158c34ef
Hania Abbasi <habbasi@MBP-Hania.local>
2022-06-14 15:35:13 -0500

    3

---
+++ Final_Exam_ISI_CS_2022.js
@@ -0,0 +1,9 @@
+const make = document.createElement("div");
+make.setAttribute("id", "new-div");
+make.innerText = "lorem ipsum"
+document.body.appendChild(make);
+
+const para = document.createElement("p");
+para.setAttribute("id", "my-final");
+para.innerText = "Hello ISI, I am done!"
+
